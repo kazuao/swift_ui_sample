616674d7abbf20fdc41cc65580256f3700f3b81b
[STV]Kazunori Aoki <k.aoki@stv-tech.co.jp>
2021-03-30 11:35:57 +0900

    add

---
+++ SwiftUISample/SwiftUiSample/Model/UserData.swift
@@ -0,0 +1,20 @@
+//
+//  UserData.swift
+//  SwiftUiSample
+//
+//  Created by Kazunori Aoki on 2021/03/30.
+//
+
+import SwiftUI
+
+struct UserData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserData_Previews: PreviewProvider {
+    static var previews: some View {
+        UserData()
+    }
+}
